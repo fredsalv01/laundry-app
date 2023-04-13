a34a94dceae8459f646b671400264e26d236a1cd
fredsalv01 <freddy.morales@usil.pe>
2023-04-12 21:22:20 -0500

    ClothesItems ScrollList

--- components/DressItems.js
+++ components/DressItems.js
@@ -1,14 +1,81 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+	Button,
+	Image,
+	Pressable,
+	ScrollView,
+	StyleSheet,
+	Text,
+	View,
+} from "react-native";
 import React from "react";
 
 const DressItems = ({ item }) => {
 	return (
 		<View>
-			<Text >{item.name}</Text>
+			<View style={styles.ClotheCard}>
+				<Image source={{ uri: item.image }} style={styles.Image} />
+				<View>
+					<Text style={styles.Name}>{item.name}</Text>
+					<Text style={styles.Price}>${item.price}</Text>
+				</View>
+				<Pressable style={styles.AddBtn}>
+					<Text style={styles.BtnText}>Add</Text>
+				</Pressable>
+			</View>
 		</View>
 	);
 };
 
 export default DressItems;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	ClotheCard: {
+		backgroundColor: "#f8f8f8",
+		margin: 14,
+		padding: 20,
+		borderRadius: 8,
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 3.84,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	Image: {
+		width: 50,
+		height: 50,
+	},
+	Name: {
+		textAlign: "center",
+		width: 83,
+		marginTop: 10,
+    marginBottom: 7,
+		fontSize: 17,
+		fontWeight: "500",
+		textTransform: "capitalize",
+	},
+	Price: {
+		textAlign: "center",
+    color: "gray",
+		fontSize: 16,
+		fontWeight: "500",
+	},
+	AddBtn: {
+		width: 80,
+	},
+	BtnText: {
+		borderColor: "gray",
+		borderWidth: 0.8,
+		textAlign: "center",
+		padding: 5,
+		marginVertical: 10,
+		borderRadius: 8,
+		color: "#088F0F",
+		fontSize: 17,
+		fontWeight: "bold",
+	},
+});
