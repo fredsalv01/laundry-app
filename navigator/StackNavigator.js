0f32a2dd4e9c2c4d1c0b610976208946ee0d415c
fredsalv01 <freddy.morales@usil.pe>
2023-06-16 17:56:23 -0500

    login screen

--- navigator/StackNavigator.js
+++ navigator/StackNavigator.js
@@ -1,16 +1,22 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "../screens/HomeScreen";
-import PickupScreen from "../screens/PickupScreen";
-import CartScreen from "../screens/CartScreen";
+import { View, Text } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import PickupScreen from '../screens/PickupScreen';
+import CartScreen from '../screens/CartScreen';
+import LoginScreen from './../screens/LoginScreen';
 
 const StackNavigator = () => {
 	const Stack = createNativeStackNavigator();
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
+				<Stack.Screen
+					name="Login"
+					component={LoginScreen}
+					options={{ headerShown: false }}
+				/>
 				<Stack.Screen
 					name="Home"
 					component={HomeScreen}
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,91 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+	StyleSheet,
+	Text,
+	View,
+	SafeAreaView,
+	TextInput,
+	KeyboardAvoidingView,
+} from 'react-native';
+import React from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useState } from 'react';
 
 const LoginScreen = () => {
-  return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
-}
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-export default LoginScreen
+	return (
+		<SafeAreaView
+			style={{
+				flex: 1,
+				backgroundColor: 'white',
+				alignItems: 'center',
+				padding: 10,
+			}}
+		>
+			<KeyboardAvoidingView>
+				<View
+					style={{
+						justifyContent: 'center',
+						alignItems: 'center',
+						marginTop: 100,
+					}}
+				>
+					<Text style={{ fontSize: 20, color: '#662d91', fontWeight: 'bold' }}>
+						Sign In
+					</Text>
 
-const styles = StyleSheet.create({})
+					<Text style={{ fontSize: 18, marginTop: 8, fontWeight: '500' }}>
+						Sign in to your account
+					</Text>
+				</View>
+
+				<View style={{ marginTop: 50 }}>
+					<View style={{ flexDirection: 'row', alignItems: 'center', gap: 13 }}>
+						<MaterialCommunityIcons
+							name="email-outline"
+							size={24}
+							color="black"
+						/>
+						<TextInput
+							placeholder="Email"
+							placeholderTextColor={'black'}
+							style={{
+								borderBottomWidth: 1,
+								borderBottomColor: 'gray',
+								width: 300,
+								marginVertical: 10,
+							}}
+							value={email}
+							onChangeText={(text) => setEmail(text)}
+						/>
+					</View>
+				</View>
+
+				<View style={{ marginTop: 40 }}>
+					<View style={{ flexDirection: 'row', alignItems: 'center', gap: 13 }}>
+						<MaterialIcons name="lock-outline" size={24} color="black" />
+						<TextInput
+							placeholder="Password"
+							placeholderTextColor={'black'}
+							style={{
+								borderBottomWidth: 1,
+								borderBottomColor: 'gray',
+								width: 300,
+								marginVertical: 10,
+							}}
+							value={password}
+							onChangeText={(text) => setPassword(text)}
+							secureTextEntry
+						/>
+					</View>
+				</View>
+			</KeyboardAvoidingView>
+		</SafeAreaView>
+	);
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({});
