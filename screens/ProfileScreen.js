7dfcc74e2c345899e79f7aa8c58cc35f95419a74
fredsalv01 <freddy.morales@usil.pe>
2023-06-19 01:25:22 -0500

    logout function

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,17 +1,46 @@
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Pressable } from "react-native";
+import {
+	StyleSheet,
+	Text,
+	View,
+	SafeAreaView,
+	TouchableOpacity,
+	Pressable,
+} from "react-native";
 import React from "react";
-
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
+import { useNavigation } from "@react-navigation/native";
 
 const ProfileScreen = () => {
-	return (
-		<SafeAreaView>
-			<View>
-        <Text>Welcome User</Text>  
-      </View>
+	const user = auth.currentUser;
+	const navigation = useNavigation();
+	const signOutUser = () => {
+		signOut(auth)
+			.then(() => {
+				navigation.replace("Login");
+			})
+			.catch((error) => {
+				Alert.alert(error.message);
+			});
+	};
 
-			<TouchableOpacity>
-        <Text>Sign Out</Text>
-      </TouchableOpacity>
+	return (
+		<SafeAreaView
+			style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+		>
+			<View>
+				<Text>Welcome {user.email}</Text>
+			</View>
+
+			<TouchableOpacity onPress={signOutUser} style={{
+        backgroundColor: "lightgrey",
+        padding: 10,
+        margin: 10,
+        marginBottom: Platform.OS === "android" ? 15 : 15,
+        borderRadius: 7,
+      }}>
+				<Text>Sign Out</Text>
+			</TouchableOpacity>
 		</SafeAreaView>
 	);
 };
